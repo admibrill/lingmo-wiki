70527d4122bdc652d6ce8ad0c4e12c2852836261
lingmo-dream <2422586862@qq.com>
2024-07-26 22:55:54 +0800

    first commit

---
+++ Wiki/index.md
@@ -0,0 +1,3 @@
+# 📒 Lingmo Wiki
+
+这里是 `Lingmo OS` 的官方 `Wiki` 页面。欢迎访问！
---
+++ metadata/index.ts
@@ -0,0 +1,17 @@
+/** 文本 */
+export const siteName = 'Lingmo OS Wiki'
+export const siteShortName = 'Lingmo OS Wiki'
+export const siteDescription = 'Lingmo OS Wiki'
+
+/** 文档所在目录 */
+export const include = ['Wiki', 'LingmoOS']
+
+/** Repo */
+export const githubRepoLink = 'https://github.com/LingmoOS'
+/** Discord */
+// export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+
+/** 无协议前缀域名 */
+export const plainTargetDomain = 'wiki.lingmo.org'
+/** 完整域名 */
+export const targetDomain = `https://${plainTargetDomain}`
