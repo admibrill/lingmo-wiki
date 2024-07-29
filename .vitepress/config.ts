0c0c428a70a464995f2b28640fd14bf1932b2e50
Walter_Fang <Walter20101209@gmail.com>
2024-07-29 19:51:50 +0800

    更新 config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -264,7 +264,7 @@ export default defineConfig({
                 link: '/en/LingmoOS/installation'
               },
               {
-                text: 'aboute',
+                text: 'about',
                 link: '/en/LingmoOS/about'
               },
             ]
