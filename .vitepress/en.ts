d2dc2814d0a4a7570652519e6201b18f69c76509
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-10-01 14:27:56 +0800

    Add sidebar

--- .vitepress/en.ts
+++ .vitepress/en.ts
@@ -3,12 +3,34 @@ import { defineConfig, type DefaultTheme } from 'vitepress'
 export const en = defineConfig({
   lang: 'en-US',
   themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' }
-    ],
+    nav: nav(),
+    sidebar: sidebarItems(),
+    editLink: {
+      pattern: 'https://github.com/LingmoOS/lingmo-wiki/edit/main/:path',
+      text: 'Edit this page on GitHub'
+    },
     footer: {
       message: 'Licensed under <a href=\"https://creativecommons.org/licenses/by-sa/4.0/deed.zh\" class=\"grabient-text\">CC BY-SA 4.0</a>.',
       copyright: 'Copyright © 2024-present Lingmo OS Team',
     }
   }
 })
+
+function nav(): DefaultTheme.NavItem[] {
+  return [
+      { text: 'Home', link: '/' }
+  ]
+}
+
+function sidebarItems(): DefaultTheme.Sidebar[] {
+  return {
+    '/wiki/': {
+      base: '/wiki/', items: [
+        {
+          text: 'About',
+          link: 'about'
+        }
+      ]
+    },
+  }
+}
